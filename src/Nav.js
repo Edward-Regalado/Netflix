97d36e2d8adc65b7eee3c6736b88df639245eb19
Edward Regalado <tregalado30@gmail.com>
2021-05-12 04:59:08 -0700

    built front-end

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import logo from "./images/NetflixLogo.png";
+import "./styles/Nav.css";
+// import { useEffect, useState } from "react";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src={logo} alt="Netflix Logo" />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
